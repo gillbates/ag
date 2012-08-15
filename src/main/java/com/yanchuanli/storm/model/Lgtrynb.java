02aabf24d36a9cfbcbc55fd6dfd077e4e7d1c7a8
Yanchuan Li <gillbates@google.com>
2012-08-15 19:37:48 +0800

    123
    Signed-off-by: Yanchuan Li <gillbates@google.com>

--- src/main/java/com/yanchuanli/storm/model/Lgtrynb.java
+++ src/main/java/com/yanchuanli/storm/model/Lgtrynb.java
@@ -7,4 +7,18 @@ package com.yanchuanli.storm.model;
  * Date: 12-8-15
  */
 public class Lgtrynb {
+    private String zklsh;
+    private String xm;
+    private int xb;
+    private String mz;
+    private String csrq;
+    private String zjlx;
+    private String zjhm;
+    private String jg;
+    private String zz;
+    private String lcsy;
+    private String fh;
+    private String rzsj;
+
+
 }
